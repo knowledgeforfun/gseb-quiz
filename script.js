b5051cebd72d51bf496c8e6ee7f2a164f3170678
knowledgeforfun <pratikkanakhara1@gmail.com>
2026-07-19 19:20:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // =======================================
 
 // ---------- Student Data ----------
-
+const API_URL = "https://script.google.com/macros/s/AKfycbwpvvDP0BQXstkO7Ga6_Fo_5tl4_prYS2ERWRyud23hXhU4GOO2jr_Id5Nx3vzUnbvqPg/exec";
 let student = {
      name: "",
     roll: "",
